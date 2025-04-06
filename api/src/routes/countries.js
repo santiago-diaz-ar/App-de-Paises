98a9108b02c16799f9bf41c1a2b3283fac8629b7
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2025-04-06 14:14:58 -0500

    base de datos gestionada avanzando en back end

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const { Router } = require("express");
+const axios = require("axios");
 
 const countriesRouter = Router();
 
-countriesRouter.get("/", async (req, res) => {
+countriesRouter.get("/countries", async (req, res) => {
   try {
-    return res.status(200).json({ funcionando: "positivo" });
+    const api = await axios.get("https://restcountries.com/v3.1/all");
+
+    console.log(api);
+    return res.status(200).json(api);
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
