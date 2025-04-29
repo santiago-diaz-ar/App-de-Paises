845b357203f3e1269fc7df289579a146cf539887
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2025-04-29 18:27:32 -0500

    modulos de front instalados proyecto funcionando con node 16

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <h1>Henry Countries Iniciando Front Javier Santiago Diaz Arcila</h1>
     </div>
   );
 }
