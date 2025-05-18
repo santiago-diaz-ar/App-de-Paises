5aba056c261409cbd2dc8af12389ae56f478829e
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2025-05-18 18:03:14 -0500

    .

---
+++ client/src/landing/Landing.jsx
@@ -0,0 +1,10 @@
+import style from "./Landing.module.css"
+
+
+function Landing() {
+  return (
+    <div className={style.container}>Primera Pagina de Desarrollo</div>
+  )
+}
+
+export default Landing
