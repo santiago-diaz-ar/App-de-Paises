6b0cbefd16f7eb6d654443e890d4a662854b55b0
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2025-05-28 17:36:59 -0500

    landign css completado

--- client/src/landing/Landing.jsx
+++ client/src/landing/Landing.jsx
@@ -4,9 +4,18 @@ import { NavLink } from "react-router-dom";
 function Landing() {
   return (
     <div className={style.containerLanding}>
-      <div className={style.cajaLanding}>App Paises</div>
       <div className={style.cajaLanding}>
-        <NavLink to={"/home"}>Iniciar sesion</NavLink>
+        <h2>Paises y Actividades</h2>
+        <h4>
+          En esta app podras localizar cualquier pais que necesites y sus
+          actividades principales, tambien podras agregar una nueva Actividad a
+          un pais si esta falta o se el Pais la implemento hace poco.
+        </h4>
+      </div>
+      <div className={style.cajaLanding1}>
+        <NavLink to={"/home"} className={style.navinicio}>
+          Iniciar sesion
+        </NavLink>
       </div>
     </div>
   );
